c9d39b95c9ccc5fb0805f07de1024cb76c1e17a3
Niv <nivdaniel99@gmail.com>
2021-04-13 17:27:55 +0300

    stam commit

--- OnlineStore/src/domain/store/store_handler.py
+++ OnlineStore/src/domain/store/store_handler.py
@@ -1,3 +1,6 @@
+from OnlineStore.src.domain.store.store import Store
+
+
 class StoreHandler:
     def __init__(self):
         self.store_dict = dict()  # key-store name value-store
@@ -22,3 +25,17 @@ class StoreHandler:
             raise Exception("store name does not exists in the system")
         if store.inventory.product_dict[product_id] is None:
             raise Exception("product id does not exists in the store")
+
+    def open_store(self, store_name, user_name):
+        if self.store_dict[store_name] is not None:
+            raise Exception("store name already exists in the system")
+        self.store_dict[store_name] = Store(store_name, user_name)
+
+    def add_new_product_to_store_inventory(self, user_name, product_details, store_name):
+        store = self.store_dict[store_name]
+        if store is None:
+            raise Exception("store name does not exists in the system")
+        store.check_permission_to_edit_store_inventory(user_name)
+        if store.inventory.product_dict[product_details["product_id"]] is not None:
+            raise Exception("product id already exists in the store")
+
