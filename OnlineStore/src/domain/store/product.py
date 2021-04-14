e4a46e52ef6d25563bf6df1b9c237a610b228983
edriyon <yonatan6671@gmail.com>
2021-04-14 19:05:02 +0300

    fffff

--- OnlineStore/src/domain/store/product.py
+++ OnlineStore/src/domain/store/product.py
@@ -2,4 +2,5 @@ class Product:
     def __init__(self,product_id, product_name: str, quantity: int ):
         self.product_name = product_name
         self.quantity = quantity
+        self.description = ""
 
