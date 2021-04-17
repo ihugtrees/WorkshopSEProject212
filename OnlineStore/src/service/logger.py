d5c4debd70c7229dcd0b4b743e7504041ed3e26d
Igor <igoralikin@gmail.com>
2021-04-17 18:19:18 +0300

    logger

---
+++ OnlineStore/src/service/logger.py
@@ -0,0 +1,16 @@
+import logging
+from datetime import datetime
+
+
+class Logger:
+    def info(self, msg):
+        dt_string = datetime.now().strftime("%d_%m_%Y")
+        logging.basicConfig(filename=f'info_{dt_string}.log', format='%(asctime)s - %(levelname)s - %(message)s',
+                            level=logging.INFO)
+        logging.info(msg)
+
+    def error(self, msg):
+        dt_string = datetime.now().strftime("%d_%m_%Y")
+        logging.basicConfig(filename=f'info_{dt_string}.log', format='%(asctime)s - %(levelname)s - %(message)s',
+                            level=logging.INFO)
+        logging.error(msg)
