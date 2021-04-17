ee39412d45f0e01bb9e549cbb6ef9b36c883c1a9
Igor <igoralikin@gmail.com>
2021-04-17 20:40:59 +0300

    deletes

--- OnlineStore/src/service/event.py
+++
@@ -1,7 +0,0 @@
-
-
-class Event:
-    def __init__(self, func_name: str, args: list, return_value):
-        self.func_name = func_name
-        self.args = args
-        self.return_value = return_value
