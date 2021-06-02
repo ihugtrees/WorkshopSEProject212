0461256129a29c13ab77675e27c559bd4f49e5bd
Niv Davidian <nivdavidian@gmail.com>
2021-06-02 13:19:56 +0300

    finish externalSsystems

--- OnlineStore/src/presentation_layer/main.py
+++ OnlineStore/src/presentation_layer/main.py
@@ -866,5 +866,5 @@ if __name__ == '__main__':
     parser.add_argument('--init_file',action='store',default="init.json",help="Initialization file")
     args = parser.parse_args()
     utils.initialize_system(file=args.init_file)
-    socketio.run(app=app, debug=True, certfile='cert.pem', keyfile='key.pem', port=8443)
+    socketio.run(app=app, debug=True, certfile='cert.pem', keyfile='key.pem', port=8443, use_reloader=False)
     # socketio.run(app=app, debug=True)
